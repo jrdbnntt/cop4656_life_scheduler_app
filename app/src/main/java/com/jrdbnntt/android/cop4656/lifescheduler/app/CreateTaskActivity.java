6b48ba7edf69d8cbe17016445bf609c3534cad1c
jrdbnntt <jrdbnntt@gmail.com>
2017-04-27 11:57:05 -0400

    Basic activity structure

---
+++ app/src/main/java/com/jrdbnntt/android/cop4656/lifescheduler/app/CreateTaskActivity.java
@@ -0,0 +1,15 @@
+package com.jrdbnntt.android.cop4656.lifescheduler.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jrdbnntt.android.cop4656.lifescheduler.R;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+    }
+}
---
+++ app/src/main/java/com/jrdbnntt/android/cop4656/lifescheduler/app/TopGoalsActivity.java
@@ -0,0 +1,31 @@
+package com.jrdbnntt.android.cop4656.lifescheduler.app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.jrdbnntt.android.cop4656.lifescheduler.R;
+
+public class TopGoalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_goals);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
